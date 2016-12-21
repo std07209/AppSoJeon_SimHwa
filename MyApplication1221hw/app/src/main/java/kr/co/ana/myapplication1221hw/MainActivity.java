618a040a15a2ca08914a080763be645fc4596a7f
KangSungJun <std07209@daum.net>
2016-12-21 18:05:40 +0900

    3

--- MyApplication1221hw/app/src/main/java/kr/co/ana/myapplication1221hw/MainActivity.java
+++ MyApplication1221hw/app/src/main/java/kr/co/ana/myapplication1221hw/MainActivity.java
@@ -2,6 +2,8 @@ package kr.co.ana.myapplication1221hw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +14,7 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     Button btn_c;
-    Button btn;
+    Button btn_r;
     EditText editText;
     TextView textView;
 
@@ -22,21 +24,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_c = (Button) findViewById(R.id.btn_c);
-        btn = (Button) findViewById(R.id.btn);
+        btn_r = (Button) findViewById(R.id.btn);
         editText = (EditText) findViewById(R.id.et_text);
         textView = (TextView) findViewById(R.id.tv);
 
         btn_c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(String.valueOf(editText.length()));
+                textView.setText(String.valueOf(editText.length()) + "글자");
             }
         });
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn_r.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editText.setText("");
+            }
+        });
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                textView.setText("0");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+               textView.setText(String.valueOf(editText.length()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
